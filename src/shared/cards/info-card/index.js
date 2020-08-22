7fe5a6ee3dced196b5c77ae06550c14dfed21269
luiscl32 <luiscl32@gmail.com>
2020-08-22 11:28:50 -0500

    icons

--- src/shared/cards/info-card/index.js
+++ src/shared/cards/info-card/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { View } from 'react-native';
 import Text from 'shared/text';
+import Icon from 'react-native-vector-icons/Entypo';
 
 import { colors as c } from 'theme/colors';
 import s from './styles';
 
 export default function InfoCard({
-  Icon = () => <> </>,
+  icon = 'rocket',
   title = '',
   description = '',
 }) {
   return (
     <View style={s.container}>
+      <Icon size={30} color={c.white} name={icon} />
       <Text
         size={16}
         color={c.light_gray}
         type={'medium'}
         align={'center'}
         mt={10}
-        mb={10}>
+        mb={5}>
         {title}
       </Text>
       <Text
